14235b7ebc445d043dc357572a9dc4a636cff25d
HimanshuChourasia <himanshuchourasia@gmail.com>
2019-03-23 02:42:34 -0400

    initial commit

--- controller/route.js
+++ controller/route.js
@@ -2,12 +2,21 @@ var express = require('express');
 var path = require('path');
 var router = express.Router();
 
+router.get('/',function (request,response) {
+    response.render(path.join(__dirname,'..','/views/index')) ;
+});
+
+
 router.get('/signIn',function (request,response) {
    response.render(path.join(__dirname,'..','/views/login')) ;
 });
 
-router.get('/signIn',function (request,response) {
-    response.render(path.join(__dirname,'..','/views/login')) ;
+router.get('/index',function (request,response) {
+    response.render(path.join(__dirname,'..','/views/index')) ;
+});
+
+router.get('/about',function (request,response) {
+    response.render(path.join(__dirname,'..','/views/about')) ;
 });
 
 module.exports = router ;
