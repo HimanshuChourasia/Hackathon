d9fb7e79cb634919ca076426aabd7e8437d4e1cd
HimanshuChourasia <himanshuchourasia@gmail.com>
2019-03-23 04:36:32 -0400

    Commit for change

--- controller/route.js
+++ controller/route.js
@@ -19,6 +19,32 @@ router.get('/about',function (request,response) {
     response.render(path.join(__dirname,'..','/views/about')) ;
 });
 
+router.get('/appointment',function (request,response) {
+    response.render(path.join(__dirname,'..','/views/appointment')) ;
+});
+
+router.get('/doctor',function (request,response) {
+    response.render(path.join(__dirname,'..','/views/doctor')) ;
+});
+
+router.get('/department',function (request,response) {
+    response.render(path.join(__dirname,'..','/views/department')) ;
+});
+
+router.get('/pricing',function (request,response) {
+    response.render(path.join(__dirname,'..','/views/pricing')) ;
+});
+
+router.get('/contact',function (request,response) {
+    response.render(path.join(__dirname,'..','/views/contact')) ;
+});
+
+
+router.get('/blog',function (request,response) {
+    response.render(path.join(__dirname,'..','/views/blog')) ;
+});
+
+
 module.exports = router ;
 
 
